ae812295f363b6da51c436bd53e66b0c566640fa
Laxmi <waghmorelaxmi@gmail.com>
2025-04-03 15:46:27 +0530

    lec 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const ele = React.createElement("h1", {}, "kkkkkkkkk")
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const ele = React.createElement("h1", {}, "waghokkkll888l 99")
         // console.log(ele)
 const root = ReactDOM.createRoot(document.getElementById("root"))
         // console.log(root)
