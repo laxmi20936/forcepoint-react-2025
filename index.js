e44b2b7a2b4bd7bd0919d8bfc467119a2adeef39
Laxmi <waghmorelaxmi@gmail.com>
2025-04-03 16:26:43 +0530

    lec 3

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-const ele = React.createElement("h1", {}, "waghokkkll888l 99")
-        // console.log(ele)
+
+const ele2 = <h1>laxmiiii wagh  inside</h1>
+// console.log(ele2)
+
+const NewComponent = () => (
+    <div>
+       <h2>I am headinggggg </h2>
+       {ele2 }     
+    </div>
+)
+console.log(NewComponent())
+console.log(<NewComponent/>)
+console.log(<NewComponent></NewComponent>)
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-        // console.log(root)
-root.render(ele)
+root.render(<NewComponent></NewComponent>)
